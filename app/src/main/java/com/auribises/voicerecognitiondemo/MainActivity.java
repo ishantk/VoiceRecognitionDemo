5f7b37faa1c723f06fbda3c41a3a3ee5768f2ef5
Ishant Kumar <ishantkumar@Ishants-Macbook-Air.local>
2017-03-31 17:27:35 +0530

    Splash Screen and SharedPreferences Commit

--- app/src/main/java/com/auribises/voicerecognitiondemo/MainActivity.java
+++ app/src/main/java/com/auribises/voicerecognitiondemo/MainActivity.java
@@ -1,24 +1,30 @@
 package com.auribises.voicerecognitiondemo;
 
 import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity implements RecognitionListener,View.OnClickListener{
 
 
     // Speech To Text Conversion
     // Text To Speech
+
+    HashMap<String,String> queAns;
 
     TextView txtWords;
     Button btnSpeak;
@@ -27,6 +33,10 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
 
     SpeechRecognizer speechRecognizer;
 
+    TextToSpeech tts;
+
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
 
     void initViews(){
         txtWords = (TextView)findViewById(R.id.textView);
@@ -40,6 +50,28 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
 
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
         speechRecognizer.setRecognitionListener(this);
+
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i == TextToSpeech.SUCCESS){
+                    Toast.makeText(MainActivity.this,"TextToSpeech Initialized",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this,"TextToSpeech Not Initialized",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        queAns = new HashMap<>();
+        queAns.put("how","I am Fine, Thank You");
+        queAns.put("duniya","Duniya Theeka Thaak Hai");
+
+        preferences = getSharedPreferences("voice",MODE_PRIVATE);
+        editor = preferences.edit();
+
+        String lastQue = preferences.getString("lastQue","Ask Any Que");
+        txtWords.setText(lastQue);
+
     }
 
 
@@ -88,13 +120,25 @@ public class MainActivity extends AppCompatActivity implements RecognitionListen
             String sentence = resultList.get(0);
             txtWords.setText(sentence);
 
-            if(sentence.contains("sms") && sentence.contains("father")){
+            /*if(sentence.toLowerCase().contains("sms") && sentence.toLowerCase().contains("father")){
 
             }else if(sentence.contains("call") && sentence.contains("mother")){
 
             }else{
 
+            }*/
+
+            if(sentence.toLowerCase().contains("how") && sentence.toLowerCase().contains("you")){
+                tts.speak("I am Fine, Thank You Very Much",TextToSpeech.QUEUE_FLUSH,null);
+            } else if(sentence.toLowerCase().contains("duniya")){
+                tts.speak("Duniya, theek thak hai",TextToSpeech.QUEUE_FLUSH,null);
+            }else {
+                tts.speak("Sorry, Kuch samajh nahi aaya",TextToSpeech.QUEUE_FLUSH,null);
             }
+
+
+            editor.putString("lastQue",sentence);
+            editor.commit();
 
         }else{
             txtWords.setText("Sorry Please Try Again");
